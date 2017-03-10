e3f981a756136a368133c5eaa1a1512033dbf816
Oleksandr Dubovyk <struykm@yandex.ru>
2017-03-10 12:59:11 +0200

    add books model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,51 @@
+# import moneyed
+# from djmoney.models.fields import MoneyField
+from mptt.models import MPTTModel, TreeForeignKey
 from django.db import models
+import datetime
+from django.utils.translation import ugettext as _
+from versatileimagefield.fields import VersatileImageField
 
-# Create your models here.
+YEAR_CHOICES = [(r, r) for r in range(1960, datetime.date.today().year + 1)]
+
+
+class Catalog(MPTTModel):
+    name = models.CharField(max_length=50, unique=True)
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    year = models.IntegerField('year', choices=YEAR_CHOICES, default=datetime.datetime.now().year)
+    pages = models.PositiveSmallIntegerField()  # 32767
+    publisher = models.ForeignKey('Publisher', on_delete=models.CASCADE, )
+    catalog = models.ForeignKey('Catalog', on_delete=models.CASCADE, )
+    cover = VersatileImageField(
+        'Image',
+        upload_to='images/'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+        # <select id="year">
+        # {% for y in range(1960, (datetime.datetime.now().year + 1)) %}
+        #     <option value="{{ y }}">{{ y }}</option>./
+        # {% endfor %}
+        # </select>
