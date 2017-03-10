2992464f896e8fc6fb63729da90fbf1e8a902031
Oleksandr Dubovyk <struykm@yandex.ru>
2017-03-10 15:50:21 +0200

    add admin.py +grappelly url

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Publisher, Book, Catalog
+from django_mptt_admin.admin import DjangoMpttAdmin
 
-# Register your models here.
+
+class CatalogAdmin(DjangoMpttAdmin):
+    pass
+
+
+admin.site.register(Book)
+admin.site.register(Catalog, CatalogAdmin)
+admin.site.register(Publisher)
