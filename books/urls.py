b02c1c59123aa4ae53e7b442495e3647bd97d1e8
Oleksandr Dubovyk <struykm@yandex.ru>
2017-04-19 03:58:30 +0300

    add books route in app

--- books/urls.py
+++ books/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 
     url(r'catalogs', views.show_catalogs, name='catalogs'),
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<book_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^$', views.index, name='books'),
+    url(r'^(?P<book_id>[0-9]+)/$', views.detail, name='book_detail'),
     url(r'^catalog/(?P<catalog_id>[0-9]+)/$', views.show_search_book_catalog, name='search_catalog'),
 ]
