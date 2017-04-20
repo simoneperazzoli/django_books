1547c8b774e8f0278c05537b180173bafd7120e0
Oleksandr Dubovyk <struykm@yandex.ru>
2017-04-20 05:16:23 +0300

    cosmetic

--- books/urls.py
+++ books/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
 
     url(r'catalogs', views.show_catalogs, name='catalogs'),
     url(r'^$', views.index, name='books'),
-    url(r'^(?P<book_id>[0-9]+)/$', views.detail, name='book_detail'),
+    url(r'^(?P<book_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^catalog/(?P<catalog_id>[0-9]+)/$', views.show_search_book_catalog, name='search_catalog'),
 ]
