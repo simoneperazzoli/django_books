576d4645435f7527311ba9c51093805032b3811e
Oleksandr Dubovyk <struykm@yandex.ru>
2017-04-20 05:34:51 +0300

    change place settings of celery

--- config/settings/base.py
+++ config/settings/base.py
@@ -56,7 +56,7 @@ DJANGO_APPS = [
     # api
     'rest_framework',
     # celery
-     'django_celery_results',
+      'django_celery_results',
 
 ]
 THIRD_PARTY_APPS = [
@@ -285,4 +285,15 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+#celery settings
+CELERY_BROKER_URL = 'amqp://guest:guest@localhost//'
 
+#: Only add pickle to this list if your broker is secured
+#: from unwanted access (see userguide/security.html)
+CELERY_ACCEPT_CONTENT = ['json']
+
+CELERY_RESULT_BACKEND = 'django-db'
+# CELERY_RESULT_BACKEND = 'django-cache'
+# CELERY_RESULT_BACKEND = 'db+sqlite:///results.sqlite'
+
+CELERY_TASK_SERIALIZER = 'json'
